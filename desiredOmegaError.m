f4032e80bd631c0fb3320e5f8e2a8a3ba03035a8
Rohith-ram-polisetti <rohithrampolisetti@gmail.com>
2026-08-16 20:45:40 +0530

    Delete desiredOmegaError.m

--- desiredOmegaError.m
+++
@@ -1,6 +0,0 @@
-function w_err_d = desiredOmegaError(q_err, K1)
-% DESIREDOMEGAERROR Computes virtual control law w_err_d = -K1 * eps_err (Eq. 13).
-    eps_err = q_err(2:4);
-    eps_err = eps_err(:);
-    w_err_d = -K1 * eps_err;
-end
